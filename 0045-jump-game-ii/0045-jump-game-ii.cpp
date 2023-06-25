e44cc6b036a65e61924d2c43ab4ee1475cfc1477
Spatika <79979665+SpatikaP@users.noreply.github.com>
2023-06-25 12:54:00 +0530

    Time: 15 ms (80.82%), Space: 16.6 MB (42.04%) - LeetHub

--- 0045-jump-game-ii/0045-jump-game-ii.cpp
+++ 0045-jump-game-ii/0045-jump-game-ii.cpp
@@ -1,14 +1,14 @@
 class Solution {
 public:
     int jump(vector<int>& nums) {
-        int farthest = 0;
-        int jump = 0;
-        int current = 0;
+        int farthest=0;
+        int current=0;
+        int jump=0;
         for(int i=0; i<nums.size()-1; i++){
-            farthest=max(farthest,i+nums[i]);
+            farthest = max(farthest,i+nums[i]);
             if(i==current){
-                current=farthest;
                 jump++;
+                current=farthest;
             }
         }
         return jump;
