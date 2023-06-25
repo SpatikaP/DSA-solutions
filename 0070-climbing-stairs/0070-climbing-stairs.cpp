63910532111601e489b3fb064dbdbfbc1d559b7a
Spatika <79979665+SpatikaP@users.noreply.github.com>
2023-06-25 13:28:09 +0530

    Time: 4 ms (13.16%), Space: 6.3 MB (14.48%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.cpp
+++ 0070-climbing-stairs/0070-climbing-stairs.cpp
@@ -1,17 +1,14 @@
 class Solution {
 public:
     int climbStairs(int n) {
-                
-        int prev1=1;
-        int prev2=0;
-        
-        for(int i=1; i<=n; i++){
-            int curr=prev1+prev2;
-            
-            prev2=prev1;
-            prev1=curr;
+        if(n<=1) return n;
+        vector<int> dp(n+1);
+        dp[0]=0;
+        dp[1]=1;
+        dp[2]=2;
+        for(int i=3; i<=n; i++){
+            dp[i]=dp[i-1]+dp[i-2];
         }
-        
-        return prev1;
+        return dp[n];
     }
 };
