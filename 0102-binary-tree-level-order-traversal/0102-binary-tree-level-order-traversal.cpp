14ce3164eecc8f892fd42222dc2cd3bac53ef09f
Spatika <79979665+SpatikaP@users.noreply.github.com>
2023-07-11 10:26:43 +0530

    Time: 12 ms (15.05%), Space: 13.6 MB (48.66%) - LeetHub

--- 0102-binary-tree-level-order-traversal/0102-binary-tree-level-order-traversal.cpp
+++ 0102-binary-tree-level-order-traversal/0102-binary-tree-level-order-traversal.cpp
@@ -17,14 +17,14 @@ public:
         queue<TreeNode*> q;
         q.push(root);
         while(!q.empty()){
+            int size=q.size();
             vector<int> level;
-            int q_size=q.size();
-            for(int i=0; i<q_size; i++){
-               TreeNode* node = q.front();
-               q.pop();
-               if(node->left!=NULL) q.push(node->left);
-               if(node->right!=NULL) q.push(node->right);
-               level.push_back(node->val);
+            for(int i=0; i<size; i++){
+                TreeNode* node=q.front();
+                q.pop();
+                if(node->left) q.push(node->left);
+                if(node->right) q.push(node->right);
+                level.push_back(node->val);
             }
             ans.push_back(level);
         }
