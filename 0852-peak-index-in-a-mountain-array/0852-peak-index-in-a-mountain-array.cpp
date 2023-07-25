8c2ed2175c9dda44e5a3708075031e48ee77d57b
Spatika <79979665+SpatikaP@users.noreply.github.com>
2023-07-25 09:08:07 +0530

    Time: 136 ms (21.45%), Space: 59.5 MB (99.79%) - LeetHub

---
+++ 0852-peak-index-in-a-mountain-array/0852-peak-index-in-a-mountain-array.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    int peakIndexInMountainArray(vector<int>& arr) {
+        int low = 0;
+        int high = arr.size();
+        int ans = 0;
+        while(low<high){
+            int mid=low+(high-low)/2;
+            if(arr[mid]>arr[mid+1]){
+                high=mid;
+                ans=mid;
+            } 
+            else{
+                low=mid+1;
+                ans=mid+1;
+            }
+            
+        }
+        return ans;
+    }
+};
