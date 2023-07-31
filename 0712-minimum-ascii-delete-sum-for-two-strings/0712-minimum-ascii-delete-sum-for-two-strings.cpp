7116a1deef811988b79ecb3341dced5bfe37e399
Spatika <79979665+SpatikaP@users.noreply.github.com>
2023-07-31 11:56:47 +0530

    Time: 35 ms (82.66%), Space: 10.8 MB (80.85%) - LeetHub

---
+++ 0712-minimum-ascii-delete-sum-for-two-strings/0712-minimum-ascii-delete-sum-for-two-strings.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    int dp[1001][1001];
+    int m,n;
+    
+    int solve(string &s1, string &s2,int i,int j){
+    
+        if(i>=m && j>=n) return 0;
+        
+        if(dp[i][j]!=-1) return dp[i][j];
+        
+        if(i>=m) return dp[i][j]=s2[j]+solve(s1,s2,i,j+1);
+        else if(j>=n) return dp[i][j]=s1[i]+solve(s1,s2,i+1,j);
+        
+        if(s1[i]==s2[j]) return dp[i][j]=solve(s1,s2,i+1,j+1);
+
+        int delete_s1_i=s1[i]+solve(s1,s2,i+1,j);
+        int delete_s2_j=s2[j]+solve(s1,s2,i,j+1);
+        
+        return dp[i][j]=min(delete_s1_i,delete_s2_j);
+    }
+    int minimumDeleteSum(string s1, string s2) {
+        m=s1.size();
+        n=s2.size();
+        memset(dp,-1, sizeof(dp));
+        return solve(s1,s2,0,0);
+    }
+};
