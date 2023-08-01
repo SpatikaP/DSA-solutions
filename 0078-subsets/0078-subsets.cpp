0b44f5480ab52aba6e200ad59167b5346e68a30c
Spatika <79979665+SpatikaP@users.noreply.github.com>
2023-08-01 11:16:43 +0530

    Time: 4 ms (48.44%), Space: 6.8 MB (98.87%) - LeetHub

--- 0078-subsets/0078-subsets.cpp
+++ 0078-subsets/0078-subsets.cpp
@@ -1,16 +1,17 @@
 class Solution {
 public:
+    void solve(vector<vector<int>> &ans, vector<int> &temp, int start, vector<int> &nums){
+        ans.push_back(temp);
+        for(int i=start; i<nums.size(); i++){
+            temp.push_back(nums[i]);
+            solve(ans,temp,i+1,nums);
+            temp.pop_back();
+        }
+    }
     vector<vector<int>> subsets(vector<int>& nums) {
         vector<vector<int>> ans;
-        ans.push_back({});
-        for(int i=0; i<nums.size(); i++){
-            int sz=ans.size();
-            for(int j=0; j<sz; j++){
-                vector<int> temp=ans[j];
-                temp.push_back(nums[i]);
-                ans.push_back(temp);
-            }
-        }
+        vector<int> temp;
+        solve(ans,temp,0,nums);
         return ans;
     }
 };
