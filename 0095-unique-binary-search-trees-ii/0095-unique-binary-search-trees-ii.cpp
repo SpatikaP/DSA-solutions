d9778f1a4ec41100ad041341633072bdafd50239
Spatika <79979665+SpatikaP@users.noreply.github.com>
2023-08-05 17:02:22 +0530

    Time: 16 ms (75.53%), Space: 15.6 MB (70.95%) - LeetHub

---
+++ 0095-unique-binary-search-trees-ii/0095-unique-binary-search-trees-ii.cpp
@@ -0,0 +1,38 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    vector<TreeNode*> solve(int start, int end){
+        if(start>end) return {NULL};
+        if(start==end) {
+            TreeNode* root = new TreeNode (start);
+            return {root};
+        }
+        vector<TreeNode*> res;
+        for(int i=start; i<=end; i++){
+            vector<TreeNode*> LBST= solve(start, i-1);
+            vector<TreeNode*> RBST = solve(i+1,end);
+            for(auto &l: LBST){
+                for(auto &r: RBST){
+                    TreeNode* root= new TreeNode(i);
+                    root->left=l;
+                    root->right=r;
+                    res.push_back(root);
+                }
+            }
+        }
+        return res;
+    }
+    vector<TreeNode*> generateTrees(int n) {
+        return solve(1,n);
+    }
+};
