e76634768cf01709badaf381cf19bc818dea0de0
Spatika <79979665+SpatikaP@users.noreply.github.com>
2023-08-13 08:21:22 +0530

    Time: 7 ms (87.95%), Space: 10 MB (44.88%) - LeetHub

---
+++ 0064-minimum-path-sum/0064-minimum-path-sum.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    int minPathSum(vector<vector<int>>& grid) {
+        int m=grid.size();
+        if(m==0) return 0;
+        int n=grid[0].size();
+        if(n==0) return 0;
+        vector<vector<int>> ans(grid);
+        for(int i=0; i<m; i++){
+            for(int j=0; j<n; j++){
+                if(i-1>=0 && j-1>=0) ans[i][j]+=min(ans[i][j-1],ans[i-1][j]);
+                else{
+                    if(j-1>=0) ans[i][j]+=ans[i][j-1];
+                    if(i-1>=0) ans[i][j]+=ans[i-1][j];
+                }                
+            }
+        }
+        return ans[m-1][n-1];
+    }
+};
