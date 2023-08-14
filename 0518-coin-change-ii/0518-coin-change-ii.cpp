257a9ce656766bb0197563b107332f5d7a0bd74f
Spatika <79979665+SpatikaP@users.noreply.github.com>
2023-08-14 17:27:00 +0530

    Time: 20 ms (53.61%), Space: 14 MB (60.78%) - LeetHub

--- 0518-coin-change-ii/0518-coin-change-ii.cpp
+++ 0518-coin-change-ii/0518-coin-change-ii.cpp
@@ -1,17 +1,17 @@
 class Solution {
 public:
     int dp[301][5001];
-    int solve(int i, vector<int> &coins, int amt){
+    int solve(int i, int amt, vector<int> &coins){
+        if(i>=coins.size()) return 0;
         if(dp[i][amt]!=-1) return dp[i][amt];
-        if(i==coins.size()) return 0;
         if(amt==0) return 1;
-        if(amt<coins[i]) return dp[i][amt]=solve(i+1,coins,amt);
-        int take=solve(i,coins,amt-coins[i]);
-        int skip=solve(i+1,coins,amt);
+        if(amt<coins[i]) return dp[i][amt]=solve(i+1,amt,coins);
+        int take=solve(i,amt-coins[i],coins);
+        int skip=solve(i+1,amt,coins);
         return dp[i][amt]=skip+take;
     }
     int change(int amount, vector<int>& coins) {
         memset(dp,-1,sizeof(dp));
-        return solve(0,coins,amount);
+        return solve(0,amount,coins);
     }
 };
