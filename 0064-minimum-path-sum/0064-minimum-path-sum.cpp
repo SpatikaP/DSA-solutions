7a1751a0f81306d4f947479d0c6d632bbd9b54a1
Spatika <79979665+SpatikaP@users.noreply.github.com>
2023-08-14 11:34:22 +0530

    Time: 7 ms (87.54%), Space: 10.1 MB (45.21%) - LeetHub

--- 0064-minimum-path-sum/0064-minimum-path-sum.cpp
+++ 0064-minimum-path-sum/0064-minimum-path-sum.cpp
@@ -8,11 +8,13 @@ public:
         vector<vector<int>> ans(grid);
         for(int i=0; i<m; i++){
             for(int j=0; j<n; j++){
-                if(i-1>=0 && j-1>=0) ans[i][j]+=min(ans[i][j-1],ans[i-1][j]);
+                if(i-1>=0 && j-1>=0){
+                    ans[i][j]+=min(ans[i-1][j],ans[i][j-1]);
+                }
                 else{
                     if(j-1>=0) ans[i][j]+=ans[i][j-1];
                     if(i-1>=0) ans[i][j]+=ans[i-1][j];
-                }                
+                } 
             }
         }
         return ans[m-1][n-1];
