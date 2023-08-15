9b4ddad93dade0551782fb0102d478417158d541
Spatika <79979665+SpatikaP@users.noreply.github.com>
2023-08-15 17:05:56 +0530

    Time: 0 ms (100.00%), Space: 6.4 MB (54.38%) - LeetHub

---
+++ 0392-is-subsequence/0392-is-subsequence.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    bool isSubsequence(string s, string t) {
+        int j=0;
+        if(s.size()==0) return true;
+        if(s.size()>t.size()) return false;
+        if(s.size()==t.size()) return s==t;
+        for(int i=0; i<t.size(); i++){
+            if(t[i]==s[j]){
+                j++;
+            }
+            if(j==s.size()) return true;
+        }
+        return false;
+    }
+};
