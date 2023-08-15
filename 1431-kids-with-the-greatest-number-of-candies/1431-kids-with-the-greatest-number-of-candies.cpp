8268d5f5f7bc7310601b9c848fc67599fa1bb07a
Spatika <79979665+SpatikaP@users.noreply.github.com>
2023-08-15 10:45:42 +0530

    Time: 3 ms (58.70%), Space: 9.1 MB (7.69%) - LeetHub

---
+++ 1431-kids-with-the-greatest-number-of-candies/1431-kids-with-the-greatest-number-of-candies.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    vector<bool> kidsWithCandies(vector<int>& candies, int extraCandies) {
+        vector<bool> ans;
+        int mx=*max_element(candies.begin(),candies.end());
+        for(int x:candies){
+            if(x+extraCandies>=mx) ans.push_back(true);
+            else ans.push_back(false);
+        }
+        return ans;
+    }
+};
