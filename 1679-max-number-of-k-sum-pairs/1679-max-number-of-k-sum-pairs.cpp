17b892323d715ab5abbc410c9008e7be0847364a
Spatika <79979665+SpatikaP@users.noreply.github.com>
2023-08-17 13:56:11 +0530

    Time: 139 ms (67.13%), Space: 72.9 MB (12.88%) - LeetHub

---
+++ 1679-max-number-of-k-sum-pairs/1679-max-number-of-k-sum-pairs.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int maxOperations(vector<int>& nums, int k) {
+        unordered_map<int,int> mp;
+        int count=0;
+        for(auto x:nums){
+            if(mp[k-x]!=0){
+                mp[k-x]--;
+                mp[x]--;
+                count++;
+            }
+            mp[x]++;
+        }
+        return count;
+    }
+};
