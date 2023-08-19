589d1a894e699a5383950545fda5a02c3f63d95e
Spatika <79979665+SpatikaP@users.noreply.github.com>
2023-08-19 13:59:28 +0530

    Time: 88 ms (56.41%), Space: 27 MB (24.51%) - LeetHub

--- 2390-removing-stars-from-a-string/2390-removing-stars-from-a-string.cpp
+++ 2390-removing-stars-from-a-string/2390-removing-stars-from-a-string.cpp
@@ -1,7 +1,7 @@
 class Solution {
 public:
     string removeStars(string s) {
-        stack<int> st;
+        stack<char> st;
         for(int i=0; i<s.size(); i++){
             if(s[i]=='*') st.pop();
             else st.push(s[i]);
