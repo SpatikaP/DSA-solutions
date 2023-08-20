2c8044a3af014ac2c29a6e76ac6c52b65bc96186
Spatika <79979665+SpatikaP@users.noreply.github.com>
2023-08-20 19:36:32 +0530

    Time: 3 ms (39.08%), Space: 6.7 MB (32.18%) - LeetHub

--- 0394-decode-string/0394-decode-string.cpp
+++ 0394-decode-string/0394-decode-string.cpp
@@ -1,35 +1,36 @@
 class Solution {
 public:
     string decodeString(string s) {
-        stack <string> sString;
-        stack <int> sInt; 
+        stack<string> sString;
+        stack<int> sNum;
         string res;
         int n=0;
         
-        for (int i=0; i<s.size(); i++) {
+        for(int i=0; i<s.size(); i++){
             if (isdigit(s[i])) {
                     n=n*10+s[i]-'0';
             }
             else if (isalpha(s[i])) {
                 res.push_back(s[i]);
             }
-            else if (s[i]=='[') {
+            else if(s[i]=='['){
                 sString.push(res);
-                sInt.push(n);
+                sNum.push(n);
                 
                 res="";
                 n=0;
             }
 
-            else if (s[i]==']'){
-                string tmp = res;
-                for(int i = 0; i < sInt.top()-1; ++i) {
-                    res += tmp;
+            else if(s[i]==']'){
+                string temp=res;
+                for(int i=0; i<sNum.top()-1; ++i){
+                    res+=temp;
                 }
-                res = sString.top() + res;
-                sString.pop(); sInt.pop();
+                res=sString.top()+res;
+                sString.pop();
+                sNum.pop();
             }
         }
-    return res;
+        return res;
     }
 };
