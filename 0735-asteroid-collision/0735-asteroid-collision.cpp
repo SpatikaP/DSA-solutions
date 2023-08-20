8924ad0478076811bf09370a3bbf72b93cfebf98
Spatika <79979665+SpatikaP@users.noreply.github.com>
2023-08-20 19:18:43 +0530

    Time: 7 ms (97.99%), Space: 17.7 MB (50.46%) - LeetHub

--- 0735-asteroid-collision/0735-asteroid-collision.cpp
+++ 0735-asteroid-collision/0735-asteroid-collision.cpp
@@ -2,19 +2,14 @@ class Solution {
 public:
     vector<int> asteroidCollision(vector<int>& as) {
         stack<int> st;
-        for(int i=0; i<as.size(); i++){
-            //positive
-            if(as[i]>0 || st.empty()) st.push(as[i]);
-            //negative
+        for(int i=0; i<as.size();i++){
+            if(st.empty() || as[i]>0) st.push(as[i]);
             else{
-                //collision
                 while(!st.empty() && st.top()>0 && st.top()<abs(as[i])) st.pop();
-                //equal and opposite
                 if(!st.empty() && st.top()>0 && st.top()==abs(as[i])) st.pop();
-                else{
-                    if(st.empty() || st.top()<0){
-                        st.push(as[i]);
-                    }
+                else if(st.empty() || st.top()<0)
+                {
+                    st.push(as[i]);
                 }
             }
         }
@@ -23,7 +18,7 @@ public:
             ans.push_back(st.top());
             st.pop();
         }
-        reverse(ans.begin(),ans.end());
+        reverse(ans.begin(),ans.end()); 
         return ans;
     }
 };
