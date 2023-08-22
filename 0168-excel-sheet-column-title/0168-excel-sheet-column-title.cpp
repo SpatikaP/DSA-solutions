e31b7705e386a563efd4671f2ac14c4e0963950e
Spatika <79979665+SpatikaP@users.noreply.github.com>
2023-08-22 12:21:41 +0530

    Time: 2 ms (49.78%), Space: 5.9 MB (37.93%) - LeetHub

--- 0168-excel-sheet-column-title/0168-excel-sheet-column-title.cpp
+++ 0168-excel-sheet-column-title/0168-excel-sheet-column-title.cpp
@@ -1,7 +1,8 @@
 class Solution {
 public:
     string convertToTitle(int n) {
-        int x=0; string s="";
+    int x=0;
+        string s="";
         while(n>0){
             int x='A'+(n-1)%26;
             s=char(x)+s;
