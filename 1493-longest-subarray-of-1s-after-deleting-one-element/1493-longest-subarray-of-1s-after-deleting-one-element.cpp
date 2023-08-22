8d7e66af2059ce64bb6c667ed6ff4cd33eb2288f
Spatika <79979665+SpatikaP@users.noreply.github.com>
2023-08-22 17:53:45 +0530

    Time: 33 ms (95.45%), Space: 46.6 MB (6.67%) - LeetHub

--- 1493-longest-subarray-of-1s-after-deleting-one-element/1493-longest-subarray-of-1s-after-deleting-one-element.cpp
+++ 1493-longest-subarray-of-1s-after-deleting-one-element/1493-longest-subarray-of-1s-after-deleting-one-element.cpp
@@ -1,25 +1,18 @@
 class Solution {
 public:
     int longestSubarray(vector<int>& nums) {
-        int ans=0; //stores the maximum length of the subarray
-        int left=0; //slinding window pointers
-        int zero=0; //counter of zero
-        int n=nums.size();
-        
-        //find the subarray with atmost one zero in it
-        for(int right=0; right<n; right++){
+        int ans=0;
+        int left=0;
+        int zero=0;
+        int right;
+        for(right=0; right<nums.size();right++){
             if(nums[right]==0) zero++;
-            while(zero>1){
-                // move the left pointer
-                if(nums[left]==0){
-                    zero--;
-                }
+            if(zero>1){
+                if(nums[left]==0) zero--;
                 left++;
-            }
-            // keep recording the lengths of all the possible subarrays and store the one with the maximum length
-            ans = max( ans, right-left);
+            } 
+            ans=max(ans,right-left);
         }
-        // if ans==n which means it contains only 1's but ATQ, you must delete one element
-        return ans==n ? ans-1 : ans;
+        return ans;
     }
 };
