0f2611a530a6f9cf6622cac3106fb7dd83af359c
Spatika <79979665+SpatikaP@users.noreply.github.com>
2023-08-30 08:23:04 +0530

    Time: 4 ms (32.45%), Space: 8.7 MB (90.23%) - LeetHub

---
+++ 0814-binary-tree-pruning/0814-binary-tree-pruning.cpp
@@ -0,0 +1,35 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    bool isOnePresent(TreeNode* node){
+        if(!node) return false;
+        if(node->val==1) return true;
+        return isOnePresent(node->left) || isOnePresent(node->right);
+    }
+    TreeNode* pruneTree(TreeNode* root) {
+        if(root==NULL) 
+            return NULL;
+        
+        if(!isOnePresent(root->left)) 
+            root->left=NULL;
+        if(!isOnePresent(root->right)) 
+            root->right=NULL;
+        
+        pruneTree(root->left);
+        pruneTree(root->right);
+        
+        if(root->val==0 && !root->left && !root->right) return NULL;
+        
+        return root;
+    }
+};
