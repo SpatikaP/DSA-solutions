54efd08c867000c1075707e8e846f707565016ed
Spatika <79979665+SpatikaP@users.noreply.github.com>
2023-09-01 08:47:41 +0530

    Time: 2 ms (49.58%), Space: 6.5 MB (55.92%) - LeetHub

--- 0216-combination-sum-iii/0216-combination-sum-iii.cpp
+++ 0216-combination-sum-iii/0216-combination-sum-iii.cpp
@@ -1,21 +1,20 @@
 class Solution {
 public:
-    void solve(int start, int end, vector<vector<int>> &ans, vector<int> &temp, int k, int n){
+    void solve(int start, int end, int k, int n, vector<int> &temp, vector<vector<int>> &ans){
         if(temp.size()==k && n==0){
             ans.push_back(temp);
             return;
         }
-        
         for(int i=start; i<=end; i++){
             temp.push_back(i);
-            solve(i+1, end, ans, temp, k, n-i);
+            solve(i+1,end,k,n-i,temp,ans);
             temp.pop_back();
         }
     }
     vector<vector<int>> combinationSum3(int k, int n) {
         vector<vector<int>> ans;
         vector<int> temp;
-        solve(1,9,ans,temp,k,n);
+        solve(1,9,k,n,temp,ans);
         return ans;
     }
 };
