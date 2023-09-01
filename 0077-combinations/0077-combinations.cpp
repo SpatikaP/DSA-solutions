7a31e87494776c149ad538c5c1a32a72770fc296
Spatika <79979665+SpatikaP@users.noreply.github.com>
2023-09-01 08:39:56 +0530

    Time: 112 ms (20.80%), Space: 59.6 MB (20.37%) - LeetHub

--- 0077-combinations/0077-combinations.cpp
+++ 0077-combinations/0077-combinations.cpp
@@ -1,21 +1,21 @@
 class Solution {
 public:
-    
-    void solve(vector<vector<int>> &ans, vector<int> &temp, int n, int k, int start){
+
+    void solve(int start, int n, int k, vector<int> &temp, vector<vector<int>> &ans){
         if(temp.size()==k){
             ans.push_back(temp);
             return;
-        }
+        } 
         for(int i=start; i<=n; i++){
             temp.push_back(i);
-            solve(ans,temp,n,k,i+1);
+            solve(i+1,n,k,temp,ans);
             temp.pop_back();
         }
     }
     vector<vector<int>> combine(int n, int k) {
         vector<vector<int>> ans;
         vector<int> temp;
-        solve(ans,temp,n,k,1);
+        solve(1,n,k,temp,ans);
         return ans;
     }
 };
