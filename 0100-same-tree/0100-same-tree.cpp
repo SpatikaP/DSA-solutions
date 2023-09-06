5ecdd206a45506f47b94cfa9e9f7abbc2821a052
Spatika <79979665+SpatikaP@users.noreply.github.com>
2023-09-06 10:30:30 +0530

    Time: 0 ms (100.00%), Space: 10.2 MB (11.11%) - LeetHub

--- 0100-same-tree/0100-same-tree.cpp
+++ 0100-same-tree/0100-same-tree.cpp
@@ -12,9 +12,28 @@
 class Solution {
 public:
     bool isSameTree(TreeNode* p, TreeNode* q) {
-        if(p==NULL || q==NULL) return p==q;
-        return p->val==q->val 
-        && isSameTree(p->left,q->left)
-        && isSameTree(p->right,q->right);
+        if(!p && !q) return true;
+        if(!p || !q) return false;
+        queue<TreeNode*> qp;
+        queue<TreeNode*> qq;
+        qp.push(p);
+        qq.push(q);
+        while(!qp.empty() && !qq.empty()){
+            TreeNode* np = qp.front(); qp.pop();
+            TreeNode* nq = qq.front(); qq.pop();
+            if(np->val!=nq->val) return false;
+            if(np->left && nq->left){
+                qp.push(np->left);
+                qq.push(nq->left);
+            }
+            else if(np->left || nq->left)  return false;
+            
+            if(np->right && nq->right){
+                qp.push(np->right);
+                qq.push(nq->right);
+            }
+            else if(np->right || nq->right)  return false;
+        }
+        return true;
     }
 };
