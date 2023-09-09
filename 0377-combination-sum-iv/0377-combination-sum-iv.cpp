bd7fae6dd74a25b91881c6cdafaa47f76cdfaee2
Spatika <79979665+SpatikaP@users.noreply.github.com>
2023-09-09 14:14:21 +0530

    Time: 67 ms (5.04%), Space: 120.6 MB (6.68%) - LeetHub

--- 0377-combination-sum-iv/0377-combination-sum-iv.cpp
+++ 0377-combination-sum-iv/0377-combination-sum-iv.cpp
@@ -1,19 +1,26 @@
 class Solution {
 public:
-    int solve(vector<int> nums, int target, vector<int> &dp ){
-        if(target==0) return 1;
-        if(dp[target]!=-1) return dp[target];
+    int t[201][1001];
+    
+    int solve(int i, vector<int> nums, int target){
         
-        dp[target]=0;
-        for(int i=0; i<nums.size(); i++){
-            if(nums[i]<=target){
-                dp[target]+=solve(nums,target-nums[i],dp);
-            }
-        }
-        return dp[target];
+        if(target==0) return 1;
+        
+        if(i>=nums.size()) return 0;
+        
+        if(target<0) return 0;
+        
+        if(t[i][target]!=-1) return t[i][target];
+        
+        int take_idx = solve(0,nums,target-nums[i]);
+        int reject_idx = solve(i+1,nums,target);
+        
+        return t[i][target]=take_idx+reject_idx;
+        
     }
+    
     int combinationSum4(vector<int>& nums, int target) {
-        vector<int> dp(target+1,-1);
-        return solve(nums,target,dp);
+        memset(t,-1,sizeof(t));
+        return solve(0,nums,target);
     }
 };
