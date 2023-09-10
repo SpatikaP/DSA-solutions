eeb242eff6a9b1455d606b82fbed99fc2b459cce
Spatika <79979665+SpatikaP@users.noreply.github.com>
2023-09-10 20:33:14 +0530

    Time: 135 ms (49.81%), Space: 94.1 MB (90.29%) - LeetHub

--- 1372-longest-zigzag-path-in-a-binary-tree/1372-longest-zigzag-path-in-a-binary-tree.cpp
+++ 1372-longest-zigzag-path-in-a-binary-tree/1372-longest-zigzag-path-in-a-binary-tree.cpp
@@ -11,11 +11,10 @@
  */
 class Solution {
 public:
-    int maxPath=0;
+    int maxSteps=0;
     void solve(TreeNode* root, int steps, bool goLeft){
-        
         if(!root) return;
-        
+        maxSteps=max(maxSteps,steps);
         if(goLeft){
             solve(root->left,steps+1,false);
             solve(root->right,1,true);
@@ -24,11 +23,10 @@ public:
             solve(root->right,steps+1,true);
             solve(root->left,1,false);
         }
-        maxPath=max(maxPath,steps);
     }
     int longestZigZag(TreeNode* root) {
         solve(root,0,true);
         solve(root,0,false);
-        return maxPath;
+        return maxSteps;
     }
 };
