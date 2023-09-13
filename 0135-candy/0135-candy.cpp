321870c2d7b45bf70d0036b24abd29ad8d1c7c26
Spatika <79979665+SpatikaP@users.noreply.github.com>
2023-09-13 11:50:47 +0530

    Time: 22 ms (32.22%), Space: 17.7 MB (72.88%) - LeetHub

--- 0135-candy/0135-candy.cpp
+++ 0135-candy/0135-candy.cpp
@@ -3,25 +3,29 @@ public:
     int candy(vector<int>& ratings) {
         int n=ratings.size();
         vector<int> left(n,1);
-        vector<int> right(n,1);
-        //left->right
+
         for(int i=1; i<n; i++){
             if(ratings[i]>ratings[i-1]){
                 left[i]=left[i-1]+1;
             }
         }
-        //right->left
+
+        int right=1;
         for(int i=n-2; i>=0; i--){
             if(ratings[i]>ratings[i+1]){
-                right[i]=right[i+1]+1;
+                right++;
+                left[i]=max(left[i],right);
+            }
+            else{
+                right=1;
             }
         }
         
         int total_candies=0;
         
-         for(int i=0; i<n; i++){
-            total_candies+=max(left[i],right[i]);
-        }
+         for(int x:left){
+            total_candies+=x;
+         }
         return total_candies;
     }
 };
