1bde8e545e620b7d8836d84d1b24eb97296db96b
Spatika <79979665+SpatikaP@users.noreply.github.com>
2023-10-16 17:53:02 +0530

    Time: 10 ms (86.12%), Space: 7.4 MB (83.08%) - LeetHub

--- 1269-number-of-ways-to-stay-in-the-same-place-after-some-steps/1269-number-of-ways-to-stay-in-the-same-place-after-some-steps.cpp
+++ 1269-number-of-ways-to-stay-in-the-same-place-after-some-steps/1269-number-of-ways-to-stay-in-the-same-place-after-some-steps.cpp
@@ -1,29 +1,26 @@
 class Solution {
 public:
-    
+    int MOD= 1e9+7;
     int n;
     int t[501][501];
-    int MOD = 1e9+7;
-    
-    int solve(int idx, int steps){
-                  
-        if(idx<0 || idx>=n) return 0;
-        if(steps==0) return idx==0; 
-        
+    int solve(int idx,int steps){
+        if(idx>=n || idx<0) return 0;
+        if(steps==0){
+            return idx==0;
+        }
         if(t[idx][steps]!=-1) return t[idx][steps];
         
         int res=solve(idx+1,steps-1);
         res=(res+solve(idx-1,steps-1))%MOD;
         res=(res+solve(idx,steps-1))%MOD;
         return t[idx][steps] = res;
-    }
-    
-    int numWays(int steps, int arrLen){
         
-        arrLen=min(arrLen,steps);
-        n=arrLen;
+
+        
+    }
+    int numWays(int steps, int arrLen) {
+        n=min(steps,arrLen);
         memset(t,-1,sizeof(t));
         return solve(0,steps);
-        
     }
 };
