fcbe96e79bfeb8cf1b1c7ff00233d5a3df266076
Spatika <79979665+SpatikaP@users.noreply.github.com>
2023-10-17 11:07:51 +0530

    Time: 62 ms (77.15%), Space: 23.8 MB (68.56%) - LeetHub

--- 2400-number-of-ways-to-reach-a-position-after-exactly-k-steps/2400-number-of-ways-to-reach-a-position-after-exactly-k-steps.cpp
+++ 2400-number-of-ways-to-reach-a-position-after-exactly-k-steps/2400-number-of-ways-to-reach-a-position-after-exactly-k-steps.cpp
@@ -1,26 +1,17 @@
 class Solution {
 public:
-    long long t[4001][1001];
     int MOD=1e9+7;
-    long long solve(int start, int end, int k){
-        
+    int t[4001][1001];
+    int solve(int start, int end, int k){
         if(k==0 && start==end) return 1;
         if(k==0) return 0;
-        
         if(t[start+1000][k]!=-1) return t[start+1000][k];
-        
-        long long res=solve(start+1,end,k-1);
-        res=(res+solve(start-1,end,k-1));
-        
-        return t[start+1000][k]=res%MOD;
+        int res=solve(start+1,end,k-1);
+        res=(res+solve(start-1,end,k-1))%MOD;
+        return t[start+1000][k]=res;
     }
     int numberOfWays(int startPos, int endPos, int k) {
-        for(int i=0; i<=3000; i++){
-            for(int j=0; j<=k; j++){
-                t[i][j]=-1;
-            }
-        }
-        long long ans = solve(startPos, endPos, k);
-        return ans%MOD;
+        memset(t,-1,sizeof(t));
+        return solve(startPos,endPos,k);
     }
 };
