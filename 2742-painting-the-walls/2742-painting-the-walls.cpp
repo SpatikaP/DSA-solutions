00fee9ece0f582cc9a2bdd95ef92991f530cb6b2
Spatika <79979665+SpatikaP@users.noreply.github.com>
2023-10-19 13:07:42 +0530

    Time: 162 ms (54.57%), Space: 90.1 MB (75.89%) - LeetHub

--- 2742-painting-the-walls/2742-painting-the-walls.cpp
+++ 2742-painting-the-walls/2742-painting-the-walls.cpp
@@ -1,12 +1,18 @@
 class Solution {
 public:
+    int n;
+    int t[501][501];
+    int solve(vector<int>& cost, vector<int>& time, int idx, int remain){
+        if(remain<=0) return 0;
+        if(idx>=n) return 1e9;
+        if(t[idx][remain]!=-1) return t[idx][remain];
+        int paint=cost[idx] + solve(cost,time,idx+1,remain-1-time[idx]);
+        int no_paint=solve(cost,time,idx+1,remain);
+        return t[idx][remain]=min(paint,no_paint);
+    }
     int paintWalls(vector<int>& cost, vector<int>& time) {
-        int n = cost.size();
-        vector<int> dp(n + 1, 1e9);
-        dp[0] = 0;
-        for (int i = 0; i < n; ++i)
-            for (int j = n; j > 0; --j)
-                dp[j] = min(dp[j], dp[max(j - time[i] - 1, 0)] + cost[i]);
-        return dp[n];
+        n=cost.size();
+        memset(t,-1,sizeof(t));
+        return solve(cost,time,0,n);
     }
 };
