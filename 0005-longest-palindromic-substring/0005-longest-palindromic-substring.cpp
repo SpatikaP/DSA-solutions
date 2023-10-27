bdcd9d65b43ed4a06725447e2f10aa710ff21402
Spatika <79979665+SpatikaP@users.noreply.github.com>
2023-10-27 13:08:41 +0530

    Time: 1036 ms (8.62%), Space: 7.3 MB (72.80%) - LeetHub

--- 0005-longest-palindromic-substring/0005-longest-palindromic-substring.cpp
+++ 0005-longest-palindromic-substring/0005-longest-palindromic-substring.cpp
@@ -1,27 +1,26 @@
 class Solution {
 public:
+    bool solve(string &s, int i, int j){
+        if(i>=j)
+            return true;
+        else if(s[i]==s[j]) 
+            return solve(s,i+1,j-1);
+        else return false;
+    }
     string longestPalindrome(string s) {
-        int n = s.size();
-        vector<vector<int>> dp(n, vector<int> (n,0));
-        string ans;
-        int len=0;
-        for(int diff=0; diff<n; diff++){
-            for(int i=0, j=i+diff; j<n; i++,j++){
-                
-                if(i==j) dp[i][j]=1;
-                else if(diff==1) dp[i][j] = s[i]==s[j] ? 2:0;
-                else{
-                    if(s[i]==s[j] && dp[i+1][j-1]) dp[i][j]=dp[i+1][j-1]+2;
-                }
-                
-                if(dp[i][j]){
-                    if(len<j-i+1){
-                        len=j-i+1;
-                        ans=s.substr(i,len);
+        int n=s.size();
+        int max_len=INT_MIN, sp = 0;
+        
+        for(int i=0; i<n; i++){
+            for(int j=i; j<n; j++){
+                if(solve(s,i,j)==true){
+                    if(j-i+1>max_len){
+                        max_len=j-i+1;
+                        sp=i;
                     }
                 }
             }
         }
-        return ans;
+        return s.substr(sp,max_len);
     }
 };
