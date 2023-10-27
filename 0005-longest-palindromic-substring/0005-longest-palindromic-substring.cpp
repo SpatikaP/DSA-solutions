07a4458dbeba829669b1797318c0abcb453cc876
Spatika <79979665+SpatikaP@users.noreply.github.com>
2023-10-27 13:11:35 +0530

    Time: 180 ms (37.97%), Space: 11.6 MB (55.92%) - LeetHub

--- 0005-longest-palindromic-substring/0005-longest-palindromic-substring.cpp
+++ 0005-longest-palindromic-substring/0005-longest-palindromic-substring.cpp
@@ -1,16 +1,16 @@
 class Solution {
 public:
+    int t[1001][1001];
     bool solve(string &s, int i, int j){
-        if(i>=j)
-            return true;
-        else if(s[i]==s[j]) 
-            return solve(s,i+1,j-1);
-        else return false;
+        if(t[i][j]!=-1) return t[i][j];
+        if(i>=j) return 1;
+        else if(s[i]==s[j]) return t[i][j]=solve(s,i+1,j-1);
+        else return 0;
     }
     string longestPalindrome(string s) {
         int n=s.size();
         int max_len=INT_MIN, sp = 0;
-        
+        memset(t,-1,sizeof(t));
         for(int i=0; i<n; i++){
             for(int j=i; j<n; j++){
                 if(solve(s,i,j)==true){
